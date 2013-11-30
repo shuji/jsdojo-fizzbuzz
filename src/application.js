1e4951f8264fa936e556e25d67fffed27c5b14fa
清野 真衣子 <maikoseino@gmail.com>
2013-11-30 16:18:48 +0900

    体力に応じてカラーコードを変更するプロパティを作成

--- src/application.js
+++ src/application.js
@@ -16,7 +16,25 @@ App.FizzBuzzController = Ember.ObjectController.extend({
 
 	answer : '',
 	resultMessage: '',
+	jemColorCode : function(){
+		// 体力が低ければ彩度を下げる
+		var _point = this.get('point');
+		var _basePoint = this.get('besePoint');
+		// 基本値
+		var r = 247,g = 189 ,b = 185;
+		var mul = _point / _basePoint;
+		r *= mul;
+		g *= mul;
+		b *= mul;
 
+		function exchangeRadix(source) {
+			var num = parseInt(source, 10);
+			var result = num.toString(16);
+			return ("0"+result).slice(-2);
+		}
+
+		return '#'+exchangeRadix(r.toString(16))+exchangeRadix(g.toString(16))+exchangeRadix(b.toString(16));
+	}.property('model.point','model.basePoint'),
 	actions: {
 		judge: function() {
 			console.debug(this.get('answer'));
